e381fc2a417272cf576d8584a72bfd56bf2fb6bc
Dominic White <domw30@gmail.com>
2019-10-16 12:44:31 +0100

    add addTodo function setting state with newTodos array

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,17 @@ type FormElem = React.FormEvent<HTMLFormElement>;
 
 function App(): JSX.Element {
   const [value, setValue] = useState<string>('');
-  const [todos, setTodos] = useState<Todo[]>([]);
+  const [todos, setTodos] = useState<Todo[]>([]); // useState is expecting an array of interface Todos
 
   const handleSubmit = (e: FormElem): void => {
     e.preventDefault(); // prevents the form from doing a refresh.
+    addTodo(value);
     setValue(''); // Turns value in input to empty string.
+  };
+
+  const addTodo = (text: string): void => {
+    const newTodos: Todo[] = [...todos, { text, complete: false }]; // spread operator brings in previous toods. new array for new todos
+    setTodos(newTodos);
   };
 
   return (
