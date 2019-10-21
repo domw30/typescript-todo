8c21579b065a6c81a306b42332df6f873a618fa5
Dominic White <domw30@gmail.com>
2019-10-21 14:54:21 +0100

    amend types back to inline types

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,5 @@
 import React, { Fragment, useState } from 'react';
 import { Todo } from '../interfaces/Todo';
-import { FormElem } from '../types/FormElem';
-import { ChangeElem } from '../types/ChangeElem';
 import {
   Wrapper,
   Heading,
@@ -14,13 +12,13 @@ function App(): JSX.Element {
   const [value, setValue] = useState<string>('');
   const [todos, setTodos] = useState<Todo[]>([]); // useState is expecting an array of interface Todos
 
-  const handleSubmit = (e: FormElem): void => {
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>): void => {
     e.preventDefault(); // prevents the form from doing a refresh.
     addTodo(value);
     setValue(''); // Turns value in input to empty string.
   };
 
-  const handleChange = (e: ChangeElem): void => {
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
     e.preventDefault(); // prevents the form from doing a refresh.
     setValue(e.target.value);
   };
--- src/types/ChangeElem.ts
+++
@@ -1,1 +0,0 @@
-export type ChangeElem = React.ChangeEvent<HTMLInputElement>;
