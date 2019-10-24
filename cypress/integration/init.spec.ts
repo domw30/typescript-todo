397086a1ed131ad51d55be74fdd3e2776ece01df
Dominic White <domw30@gmail.com>
2019-10-24 23:51:17 +0100

    remove example tests: only test Apps you control

--- cypress/integration/init.spec.ts
+++ cypress/integration/init.spec.ts
@@ -1,13 +1,1 @@
 /// <reference types="cypress" />
-
-describe('Example', () => {
-  it('Example task', () => {
-    cy.visit('http://example.com');
-  });
-});
-
-describe('Flexbox Froggy Link', () => {
-  it('directs user to Flexbox Froggy', () => {
-    cy.visit('http://flexboxfroggy.com');
-  });
-});
