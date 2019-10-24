a4765d9526f44ee8e60517d067403aad17812335
Dominic White <domw30@gmail.com>
2019-10-24 10:36:39 +0100

    add colors to icons

--- src/styles/icons.tsx
+++ src/styles/icons.tsx
@@ -1,15 +1,13 @@
 import styled from 'styled-components';
 import { DeleteBin2 } from 'styled-icons/remix-fill/DeleteBin2';
-import { CheckboxChecked } from 'styled-icons/boxicons-regular/CheckboxChecked';
+import { Check } from 'styled-icons/octicons/Check';
 
 export const DeleteIcon = styled(DeleteBin2)`
-  color: black;
+  color: #fe7575;
   height: 1.5rem;
-  width: 1.5rem;
 `;
 
-export const CheckIcon = styled(CheckboxChecked)`
-  color: black;
-  height: 2rem;
-  width: 2rem;
+export const CheckIcon = styled(Check)`
+  color: #8cd896;
+  height: 1.5rem;
 `;
