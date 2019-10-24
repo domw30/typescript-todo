cc15f84c3e975c946a703e6382e238df65a13f1f
Dominic White <domw30@gmail.com>
2019-10-24 10:13:04 +0100

    change DeleteIcon for removedTodo

--- src/styles/icons.tsx
+++ src/styles/icons.tsx
@@ -1,16 +1,22 @@
 import styled from 'styled-components';
-import { Delete } from 'styled-icons/feather/Delete';
+import { DeleteBin2 } from 'styled-icons/remix-fill/DeleteBin2';
 import { CheckboxChecked } from 'styled-icons/boxicons-regular/CheckboxChecked';
 import { CheckboxUnchecked } from 'styled-icons/icomoon/CheckboxUnchecked';
 
-export const DeleteIcon = styled(Delete)`
+export const DeleteIcon = styled(DeleteBin2)`
   color: black;
+  height: 1.5rem;
+  width: 1.5rem;
 `;
 
 export const Check = styled(CheckboxChecked)`
   color: black;
+  height: 2rem;
+  width: 2rem;
 `;
 
 export const Uncheck = styled(CheckboxUnchecked)`
   color: black;
+  height: 2rem;
+  width: 2rem;
 `;
