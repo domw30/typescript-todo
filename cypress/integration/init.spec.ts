77bcdb9be1ff6a99f74fe3f798f0dd77e6bfd765
Dominic White <domw30@gmail.com>
2019-10-25 10:38:05 +0100

    Cypress - add and pass test for existing input field

--- cypress/integration/init.spec.ts
+++ cypress/integration/init.spec.ts
@@ -1,7 +1,17 @@
 /// <reference types="cypress" />
 
 describe('Todo List', () => {
-  it('Visits the Todo List single page app', () => {
+  it('visits the Todo List single page app', () => {
     cy.visit('http://localhost:3000/');
+  });
+
+  it('finds the content "Todo List"', () => {
+    cy.visit('http://localhost:3000/');
+    cy.contains('Todo List');
+  });
+
+  it('clicks the input field "Enter Todo"', () => {
+    cy.visit('http://localhost:3000/');
+    cy.get('input');
   });
 });
