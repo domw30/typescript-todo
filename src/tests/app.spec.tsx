011318a734517c8c5075028ff0cd78fb731e9362
Dominic White <domw30@gmail.com>
2019-10-29 13:22:21 +0000

    J/E tests - practice shallow testing with snapshots

--- src/tests/app.spec.tsx
+++ src/tests/app.spec.tsx
@@ -1,11 +1,32 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import App from '../components/app';
+import renderer from 'react-test-renderer';
+import Header from '../components/app';
+import TodoForm from '../components/app';
 
 describe('First React component test with Enzyme', () => {
   it('renders without crashing', () => {
     shallow(<App />);
     // shallow assertion method. Igonres any child components. E.g. Header will be ignored.
     // A typed of unit test for React, designed to test one function.
+    // smoke test. Verifies a component renders without crashing/throwing.
+  });
+});
+
+describe('First Snapshot test', () => {
+  it('app should render correctly', () => {
+    const app = renderer.create(<App />).toJSON();
+    expect(app).toMatchSnapshot();
+  });
+
+  it('should render header correctly', () => {
+    const header = shallow(<Header />);
+    expect(header).toMatchSnapshot();
+  });
+
+  it('should render TodoForm correctly', () => {
+    const todoForm = shallow(<TodoForm />);
+    expect(todoForm).toMatchSnapshot();
   });
 });
