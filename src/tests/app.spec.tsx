e5ebb8a79d23a8ef986d8728f62cee406f6ad1bc
Dominic White <domw30@gmail.com>
2019-10-30 13:33:01 +0000

    J/E tests - add and pass test for adding a todo and updating the input value

--- src/tests/app.spec.tsx
+++ src/tests/app.spec.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import { render, mount } from 'enzyme';
+import { render, mount, shallow } from 'enzyme';
 import App from '../components/app';
+import { TodoInput, TodoForm, TodoListItem } from '../styles/styles';
 
 describe('First component test with Enzyme', () => {
   it('renders without crashing', () => {
@@ -17,7 +18,27 @@ describe('First component snapshot test', () => {
 
 describe('static rendering tests', () => {
   it('renders the heading Todo List', () => {
-    const wrapper = render(<App />);
+    const wrapper = shallow(<App />);
     expect(wrapper.text()).toContain('Todo List');
+  });
+});
+
+describe('App', () => {
+  describe('when a user adds a Todo into the todo input', () => {
+    it('should update the input value state property', () => {
+      const mockPreventDefault = jest.fn();
+      const wrapper = shallow(<App />);
+      wrapper.find(TodoInput).simulate('change', {
+        preventDefault: mockPreventDefault,
+        target: { value: 'Test Todo' },
+      });
+      wrapper
+        .find(TodoForm)
+        .simulate('submit', { preventDefault: mockPreventDefault });
+
+      const result = wrapper.find(TodoListItem).text();
+
+      expect(result).toBe('Test Todo');
+    });
   });
 });
