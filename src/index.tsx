f96e69393c98cf6939f7ce969bb66ced6758c5cf
Dominic White <domw30@gmail.com>
2019-11-13 15:01:18 +0000

    wrap themeprovider around App component

--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from 'components/app';
 import * as serviceWorker from 'serviceWorker';
+import { ThemeProvider } from 'styled-components';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <ThemeProvider theme={{ mode: 'light' }}>
+    <App />
+  </ThemeProvider>,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
